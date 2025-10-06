ebc134eda516adcd3f1d4c246126d20dc91304e9
Rama200000 <gederamajayakusuma@gmail.com>
2025-10-06 10:18:04 +0700

    chore: hapus index.html, style.css, script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document
-  .getElementById("formPendaftaran")
-  .addEventListener("submit", function (e) {
-    e.preventDefault(); // cegah reload
-
-    alert("Pendaftaran berhasil dikirim!\nTerima kasih sudah mendaftar.");
-  });
